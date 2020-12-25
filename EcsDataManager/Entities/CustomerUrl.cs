cf9e5042e1c6880ce12f8927b06d2232eb21ebf0
pdotf <76215919+pdotf@users.noreply.github.com>
2020-12-25 18:22:20 +0330

    CustomerUrl Model

---
+++ EcsDataManager/Entities/CustomerUrl.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EcsDataManager.Entities
+{
+    public class CustomerUrl
+    {
+        public int id { get; set; }
+        public int customerId { get; set; }
+        public string link { get; set; }
+        public Int16 isMain { get; set; }
+
+    }
+}
