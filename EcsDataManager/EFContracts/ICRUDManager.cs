3d473bdf114a8cba8fb6203d06f5cd65e8b88185
Behrad87 <bdotz@outlook.com>
2020-12-31 16:48:53 +0330

    EF Contracts for crud added.not functional yet

---
+++ EcsDataManager/EFContracts/ICRUDManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcsDataManager.EFContracts
+{
+    public interface ICRUDManager<TEntityDto, TEntityViewDto, TId> 
+    {
+        Task Add(TEntityDto dto);
+        Task Update(TEntityDto dto);
+        Task Delete(TId id);
+        List<TEntityViewDto> GetAll(int currentPage, int rowPerPage, string search);
+        TEntityDto Get(TId id);
+    }
+}
