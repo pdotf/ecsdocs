4bae2e9f6cccb5f12285ed9aee0c13b598625540
Behrad87 <bdotz@outlook.com>
2021-01-03 15:50:36 +0330

    Device delete issue resolved by adding new inteface.

--- EcsDataManager/EFContracts/ICRUDManager.cs
+++ EcsDataManager/EFContracts/ICRUDManager.cs
@@ -5,10 +5,11 @@ using System.Threading.Tasks;
 
 namespace EcsDataManager.EFContracts
 {
-    public interface ICRUDManager<TEntity>
+    public interface ICRUDManager<TEntity> 
     {
         Task<List<TEntity>> GetAll(short ismain=0);
         Task<TEntity> Get(int id,short ismain=0);
+        
         Task<int> Add(TEntity entity,short ismain=0);
         Task<int> Change(TEntity dbEntity, TEntity entity);
         Task<int> Delete(TEntity entity);
---
+++ EcsDataManager/EFContracts/IDeviceManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace EcsDataManager.EFContracts
+{
+    public interface IDeviceManager<T>
+    {
+        Task<T> GetDevice(int id, short ismain = 0);
+    }
+}
